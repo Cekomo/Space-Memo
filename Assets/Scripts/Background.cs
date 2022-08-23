2e39e019375fd0520f4b3b9d9c16db95a89620e5
cekomo <cemil.sahin@ozu.edu.tr>
2022-08-23 17:42:48 +0300

    spaceship movement initialization

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Background : MonoBehaviour
+public class BackGround : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public SpaceCraft spaceCraft;
+    
+    public GameObject backGround; // to represent background
+    private float bgSpeed = 0.3f; // to adjust the background speed considering space-craft
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // I would like to set the speed of background slightly less than spacecraft since..
+        //.. stars are far away and the movement should seem realistic
+        transform.Translate(Vector2.up * (spaceCraft.currentVelocity * 0.7f) * Time.deltaTime);
+
+        // (0.3 * 5) to make it slightly less than spacecraft moving speed
+        // should have a view like the spacecraft travels among stars 
+        if (spaceCraft.isRight) // to move the background wrt spacecraft towards right
+            transform.Translate(Vector2.right * bgSpeed * 5 * Time.deltaTime);
+        if (spaceCraft.isLeft) // to move the background wrt spacecraft towards left
+            transform.Translate(Vector2.right * -bgSpeed * 5 * Time.deltaTime);
     }
 }
--- Assets/Scripts/SpaceCraft.cs
+++ Assets/Scripts/SpaceCraft.cs
@@ -11,20 +11,24 @@ public class SpaceCraft : MonoBehaviour
     
     //private int pD = 100; // pixel distance to detect the swipe action
     private Vector2 startPos; // first touch / mouse press
-    private float moveX; // slide operation on x-axis
-    private float moveY; // slide operation on y-axis
-    private bool isLeft; // move the spacecraft left if the variable is true
-    private bool isRight; // move the spacecraft right if the variable is true
+    [HideInInspector] public bool isLeft; // move the spacecraft left if the variable is true
+    [HideInInspector] public bool isRight; // move the spacecraft right if the variable is true
+    private bool isUp; // force up the spacecraft if the variable is true
 
     private int screenX = Screen.width; // to determine swipe action on x-axis
     private int screenY = Screen.height; // to determine swipe action on y-axis 
     private float moveClock; // determines the flight time for left/right 
-    public float moveSpeed;
+    public float moveSpeed; // to determine speed constant of spacecraft
+    [HideInInspector] public float currentVelocity; // to check 
+
+    Rigidbody2D p_RigidBody; // rigidbody of the player 
 
     void Start()
     {
         isLeft = false; isRight = false;
         moveClock = 0f;
+
+        p_RigidBody = GetComponent<Rigidbody2D>();
     }
 
     
@@ -44,41 +48,54 @@ public class SpaceCraft : MonoBehaviour
             if (moveClock <= 0f)
                 isLeft = false;
         }
+        else if (isUp) // be careful, it is FORCE, not just move operation
+        {
+            p_RigidBody.AddForce(transform.up * moveSpeed / 10);
+            moveClock -= Time.deltaTime;
+            if (moveClock <= 0f)
+                isUp = false;
+
+            currentVelocity = p_RigidBody.velocity.magnitude;
+        }
 
         // can be tried on mobile and desktop mobile simulation
         if (Input.touchCount > 0) // to handle index out of bound error
             if (Input.GetTouch(0).phase == TouchPhase.Began)  startPos = Input.touches[0].position; 
 
-        //if (Input.touchCount > 0) // if there are multiple touches on the screen
-        //{ // determine their direction and magnitude
-        //    moveX = Input.GetTouch(0).deltaPosition.x;
-        //    //moveY = Input.GetTouch(0).deltaPosition.y;
-        //}
-
         // if statements to adjust direction and flight time
-        if (Input.touchCount > 0 && !isRight && !isLeft) // to handle index out of bound error
-        {
-            if (Input.touches[0].position.x - startPos.x >= screenX / 2) // go right statement
+        if (Input.touchCount > 0 && !isRight && !isLeft && !isUp && Input.GetTouch(0).phase == TouchPhase.Ended) // to handle index out of bound error
+        { // transform.position in the if statement is to limit the movement boundary of the player
+            if (Input.touches[0].position.x - startPos.x >= screenX / 1.75f && player.transform.position.x < 1.75f) // go right statement
             {
                 moveClock = 1f;
                 isRight = true;
             }
-            else if (Input.touches[0].position.x - startPos.x >= screenX / 4) // go right statement
+            else if (Input.touches[0].position.x - startPos.x >= screenX / 6 & player.transform.position.x < 1.75f) // go right statement
             {
                 moveClock = 0.5f;
                 isRight = true;
             }
-            else if (Input.touches[0].position.x - startPos.x <= -screenX / 2) // go left statement
+            else if (Input.touches[0].position.x - startPos.x <= -screenX / 1.75f && player.transform.position.x > -1.75f) // go left statement
             {
                 moveClock = 1f;
                 isLeft = true;
             }
-            else if (Input.touches[0].position.x - startPos.x <= -screenX / 4) // go left statement
+            else if (Input.touches[0].position.x - startPos.x <= -screenX / 6 && player.transform.position.x > -1.75f) // go left statement
             {
                 moveClock = 0.5f;
                 isLeft = true;
             }
-            //print(Input.touches[0].position.x - startPos.x);
+            else if (Input.touches[0].position.y - startPos.y >= -screenY / 1.75f) // force up if swipe is along half of the screen
+            {
+                moveClock = 1f;
+                isUp = true;
+            }
+            else if (Input.touches[0].position.y - startPos.y >= -screenY / 6) // force up if swipe is along one sixth the screen
+            {
+                moveClock = 0.5f;
+                isUp = true;
+            }
+            print(player.transform.position.x);
         }
     }
 }
