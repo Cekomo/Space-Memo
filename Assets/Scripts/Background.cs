66c87eddda3433b205a9c524bf55e81e643ceea0
Cekomo <cemil.sahin@ozu.edu.tr>
2022-08-25 20:24:48 +0300

    x-axis border returning mechanism

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -8,7 +8,7 @@ public class BackGround : MonoBehaviour
     
     public GameObject backGround; // to represent background
     private Rigidbody2D bg_Rigidbody; // to adjust the position of the background
-    private float bgSpeed = 0.3f; // to adjust the background speed considering space-craft
+    //private float bgSpeed = 0.3f; // to adjust the background speed considering space-craft
 
     void Start()
     {
@@ -26,12 +26,13 @@ public class BackGround : MonoBehaviour
 
             // (0.3f * 5) to make it slightly less than spacecraft moving speed
             // should have a view like the spacecraft travels among stars 
-            if (spaceCraft.isRight && (spaceCraft.player.transform.position.x < 1.75f || spaceCraft.player.transform.position.x < -1.75f)) 
-                // to force the background wrt spacecraft towards right and enable counter movement when boundary is exceeded
-                bg_Rigidbody.AddForce(transform.right * bgSpeed * 2f * 0.8f); // bgSpeed * 2 = moveSpeed / 3 
-            if (spaceCraft.isLeft && (spaceCraft.player.transform.position.x > -1.75f || spaceCraft.player.transform.position.x > 1.75f))
-                // to force the background wrt spacecraft towards left and enable counter movement when boundary is exceeded
-                bg_Rigidbody.AddForce(transform.right * -bgSpeed * 2f * 0.8f);
+            //if (spaceCraft.isRight && (spaceCraft.player.transform.position.x < 1.75f || spaceCraft.player.transform.position.x < -1.75f)) 
+            //    // to force the background wrt spacecraft towards right and enable counter movement when boundary is exceeded
+            //    bg_Rigidbody.AddForce(transform.right * bgSpeed * 2f * 0.8f); // bgSpeed * 2 = moveSpeed / 3 
+            //if (spaceCraft.isLeft && (spaceCraft.player.transform.position.x > -1.75f || spaceCraft.player.transform.position.x > 1.75f))
+            //    // to force the background wrt spacecraft towards left and enable counter movement when boundary is exceeded
+            //    bg_Rigidbody.AddForce(transform.right * -bgSpeed * 2f * 0.8f);
+
 
             // to prevent the stops for counter movement on boundaries 
             //if (spaceCraft.player.transform.position.x > 1.75f && spaceCraft.isLeft)
@@ -42,15 +43,16 @@ public class BackGround : MonoBehaviour
             //{ // to zero the speed when touch finger up or the x-axis boundary is exceeded
             //    bg_Rigidbody.velocity = new Vector2(0, spaceCraft.currentVelocity * 0.7f * Time.deltaTime);
             //}
-            print(bg_Rigidbody.velocity.x);
+
             // to prevent the stops for counter movement on boundaries and stop otherwise
-            if (Input.touchCount == 0 || (spaceCraft.player.transform.position.x > 1.75f && !spaceCraft.isLeft))
-                bg_Rigidbody.velocity = new Vector2(spaceCraft.speedFading, spaceCraft.currentVelocity * 0.7f * Time.deltaTime);
+            //if (Input.touchCount == 0 || (spaceCraft.player.transform.position.x > 1.75f && !spaceCraft.isLeft))
+            //    bg_Rigidbody.velocity = new Vector2(spaceCraft.speedFading, spaceCraft.currentVelocity * 0.7f * Time.deltaTime);
             
-            else if (Input.touchCount == 0 || (spaceCraft.player.transform.position.x < -1.75f && !spaceCraft.isRight))                          
-                bg_Rigidbody.velocity = new Vector2(spaceCraft.speedFading, spaceCraft.currentVelocity * 0.7f * Time.deltaTime);
-            
+            //else if (Input.touchCount == 0 || (spaceCraft.player.transform.position.x < -1.75f && !spaceCraft.isRight))                          
+            //    bg_Rigidbody.velocity = new Vector2(spaceCraft.speedFading, spaceCraft.currentVelocity * 0.7f * Time.deltaTime);
         }
 
+        if (spaceCraft.isFinished) // to prevent background slipping after collision
+            bg_Rigidbody.velocity = new Vector2(0, 0);
     }
 }
--- Assets/Scripts/SpaceCraft.cs
+++ Assets/Scripts/SpaceCraft.cs
@@ -14,14 +14,14 @@ public class SpaceCraft : MonoBehaviour
     
     //private int pD = 100; // pixel distance to detect the swipe action
     private Vector2 startPos; // first touch / mouse press
-    [HideInInspector] public bool isLeft; // move the spacecraft left if the variable is true
-    [HideInInspector] public bool isRight; // move the spacecraft right if the variable is true
+    //[HideInInspector] public bool isLeft; // move the spacecraft left if the variable is true
+    //[HideInInspector] public bool isRight; // move the spacecraft right if the variable is true
     //private bool isUp; // force up the spacecraft if the variable is true
     private bool isRightLeft; // first up function must be triggered to go left/right
+    private bool isCounterMove; // to return back of the spacecraft when it exceeds borders
 
     private int screenX = Screen.width; // to determine swipe action on x-axis
     private int screenY = Screen.height; // to determine swipe action on y-axis 
-    //private float moveClock; // determines the flight time for left/right 
     
     public float moveSpeed; // to determine speed constant of spacecraft
     [HideInInspector] public float currentVelocity; // to check the velocity of the spacecraft
@@ -34,14 +34,23 @@ public class SpaceCraft : MonoBehaviour
 
     void Start()
     {
-        isLeft = false; isRight = false; isRightLeft = false;
-        //moveClock = 0f;
+        currentVelocity = 0f; // velocity of spacecraft
+        
+        //isLeft = false; isRight = false; 
+        isRightLeft = false; isCounterMove = false;
 
         p_RigidBody = GetComponent<Rigidbody2D>();
         sr = gameObject.GetComponent<SpriteRenderer>();
+        mainCamera = gameObject.GetComponent<Camera>();
     }
 
-    
+    void FixedUpdate()
+    { 
+        // to adjust the speed of camera on y-axis
+        if (!isFinished) // spacecraft is shaking when camera movement is in update() function
+            Camera.main.transform.Translate(Vector2.up * currentVelocity * Time.deltaTime);
+    }
+
     void Update()
     {
         if (Input.touchCount > 0 && !isFinished) // to handle index out of bound error
@@ -50,19 +59,19 @@ public class SpaceCraft : MonoBehaviour
          
         if (Input.touchCount > 0 && !isFinished)
         {
-            if (Input.touches[0].position.x - startPos.x >= screenX / 3 && player.transform.position.x < 1.75f && isRightLeft)
+            if (Input.touches[0].position.x - startPos.x >= screenX / 5 && player.transform.position.x < 1.7f && isRightLeft)
             {
                 p_RigidBody.AddForce(transform.right * moveSpeed / 3);
                 //transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
-                isRight = true;
+                //isRight = true;
             }
-            else if (Input.touches[0].position.x - startPos.x <= -screenX / 3 && player.transform.position.x > -1.75f && isRightLeft)
+            else if (Input.touches[0].position.x - startPos.x <= -screenX / 5 && player.transform.position.x > -1.7f && isRightLeft)
             {
                 p_RigidBody.AddForce(transform.right * -moveSpeed / 3);
                 //transform.Translate(Vector2.right * -moveSpeed * Time.deltaTime);
-                isLeft = true;
+                //isLeft = true;
             }
-            else if (Input.touches[0].position.y - startPos.y >= screenY / 4 && player.transform.position.y < 32f) // force up if swipe is along one sixth the screen
+            else if (Input.touches[0].position.y - startPos.y >= screenY / 6 && player.transform.position.y < 32f) // force up if swipe is along one sixth the screen
             {
                 p_RigidBody.AddForce(transform.up * moveSpeed / 3);
 
@@ -71,25 +80,39 @@ public class SpaceCraft : MonoBehaviour
             }
         }
 
-
-        if (Input.touchCount == 0 || Mathf.Abs(player.transform.position.x) > 1.75f && !isFinished)
+        if (Input.touchCount == 0 || Mathf.Abs(player.transform.position.x) > 1.7f && !isFinished)
         {
-            speedFading = p_RigidBody.velocity.x * 0.985f; // speed decreases cumulatively (multiplied with 0.98 continuously)
+            speedFading = p_RigidBody.velocity.x * 0.96f; // speed decreases cumulatively (multiplied with 0.98 continuously)
             p_RigidBody.velocity = new Vector2(speedFading, currentVelocity); // omitting time.deltatime solves background speed problem
             //transform.Translate(Vector2.up * currentVelocity * Time.deltaTime);
-            isRight = false; isLeft = false;
+            //isRight = false; isLeft = false;
+            if (p_RigidBody.velocity.x < 0.001f) // slightly more than zero since the velocity never be zero (always infinitely small greater)
+                isCounterMove = true; // make the counter move available if velocity is zero out of the border
         }
 
-        if (player.transform.position.y > 32 && !isFinished) // Y: 32 is the maximum vertical distance for map
-        { // if spacecraft exceeds the border, switch the camera to stop the view
+        if (Input.touchCount == 0 && isCounterMove && Mathf.Abs(player.transform.position.x) > 1.7f)
+        { // if the spacecraft exceeds the border on x-axis, turn back inside of the border
+            if (player.transform.position.x < -1.7f) // if left border exceeds
+                transform.Translate(Vector2.right * 0.5f * Time.deltaTime);
+            else if (player.transform.position.x > 1.7f) // if right border exceeds
+                transform.Translate(Vector2.right * -0.5f * Time.deltaTime);
 
-            cameraPos = mainCamera.transform.position;
-            finishCamera.transform.position = cameraPos;
-            //print("f: " + finishCamera.transform.position.ToString());
-            
-            mainCamera.enabled = false;
-            finishCamera.enabled = true;
+            if (Mathf.Abs(player.transform.position.x) < 1.7f)
+                isCounterMove = false; // turning back lasts until coordinate < |1.7f|
+        }
+        
+        if (player.transform.position.y > 32 && !isFinished) // Y: 32 is the maximum vertical distance for map
+        { 
             isFinished = true;
+
+            // this functions were needed where finishcamera replaced the main camera
+            // if spacecraft exceeds the border, switch the camera to stop the view
+            //cameraPos = mainCamera.transform.position;
+            //finishCamera.transform.position = cameraPos;
+            //print("f: " + finishCamera.transform.position.ToString());
+
+            //mainCamera.enabled = false;
+            //finishCamera.enabled = true;
         }
     }
 
@@ -102,93 +125,4 @@ public class SpaceCraft : MonoBehaviour
             p_RigidBody.bodyType = RigidbodyType2D.Static; // to stop the object and the camera to stop the view
         }
     }
-
-    //void Update()
-    //{
-    //    if (isRight)
-    //    {
-    //        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
-    //        moveClock -= Time.deltaTime;
-    //        if (moveClock <= 0f)
-    //            isRight = false;
-    //    }
-    //    else if (isLeft)
-    //    {
-    //        transform.Translate(Vector2.right * -moveSpeed * Time.deltaTime);
-    //        moveClock -= Time.deltaTime;
-    //        if (moveClock <= 0f)
-    //            isLeft = false;
-    //    }
-    //    else if (isUp) // be careful, it is FORCE, not just move operation
-    //    {
-    //        p_RigidBody.AddForce(transform.up * moveSpeed / 10);
-    //        moveClock -= Time.deltaTime;
-    //        if (moveClock <= 0f)
-    //            isUp = false;
-
-    //        isRightLeft = true;
-    //        currentVelocity = p_RigidBody.velocity.magnitude;
-    //    }
-
-    //    // can be tried on mobile and desktop mobile simulation
-    //    if (Input.touchCount > 0 && !isFinished) // to handle index out of bound error
-    //        if (Input.GetTouch(0).phase == TouchPhase.Began) startPos = Input.touches[0].position;
-
-    //    // if statements to adjust direction and flight time
-    //    if (Input.touchCount > 0 && !isRight && !isLeft && !isUp && Input.GetTouch(0).phase == TouchPhase.Ended && !isFinished)
-    //    { // transform.position in the if statement is to limit the movement boundary of the player
-    //        if (Input.touches[0].position.x - startPos.x >= screenX / 1.75f && player.transform.position.x < 1.75f && isRightLeft) // go right statement
-    //        {
-    //            moveClock = 1f;
-    //            isRight = true;
-    //        }
-    //        else if (Input.touches[0].position.x - startPos.x >= screenX / 6 & player.transform.position.x < 1.75f && isRightLeft) // go right statement
-    //        {
-    //            moveClock = 0.5f;
-    //            isRight = true;
-    //        }
-    //        else if (Input.touches[0].position.x - startPos.x <= -screenX / 1.75f && player.transform.position.x > -1.75f && isRightLeft) // go left statement
-    //        {
-    //            moveClock = 1f;
-    //            isLeft = true;
-    //        }
-    //        else if (Input.touches[0].position.x - startPos.x <= -screenX / 6 && player.transform.position.x > -1.75f && isRightLeft) // go left statement
-    //        {
-    //            moveClock = 0.5f;
-    //            isLeft = true;
-    //        }
-    //        else if (Input.touches[0].position.y - startPos.y >= -screenY / 1.75f && player.transform.position.y < 32f) // force up if swipe is along half of the screen
-    //        {
-    //            moveClock = 1f;
-    //            isUp = true;
-    //        }
-    //        else if (Input.touches[0].position.y - startPos.y >= -screenY / 6 && player.transform.position.y < 32f) // force up if swipe is along one sixth the screen
-    //        {
-    //            moveClock = 0.5f;
-    //            isUp = true;
-    //        }
-    //    }
-
-    //    if (player.transform.position.y > 32 && !isFinished) // Y: 32 is the maximum vertical distance for map
-    //    { // if spacecraft exceeds the border, switch the camera to stop the view
-
-    //        cameraPos = mainCamera.transform.position;
-    //        finishCamera.transform.position = cameraPos;
-    //        //print("f: " + finishCamera.transform.position.ToString());
-
-    //        mainCamera.enabled = false;
-    //        finishCamera.enabled = true;
-    //        isFinished = true;
-    //    }
-    //}
-
-    //void OnCollisionEnter2D(Collision2D collision)
-    //{ // when collision happens, the camera sometimes go out the borders of the background
-    //    if (collision.gameObject.tag == "Obstacle")
-    //    { // if the player hit an object, game over
-    //        isFinished = true;
-    //        sr.enabled = false;
-    //        p_RigidBody.bodyType = RigidbodyType2D.Static; // to stop the object and the camera to stop the view
-    //    }
-    //}
 }
