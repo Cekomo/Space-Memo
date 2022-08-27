dc3637ebfa55b7fda7395290a732cc51d23c8cde
Cekomo <cemil.sahin@ozu.edu.tr>
2022-08-27 20:45:16 +0300

    rocket launcher mechanism bug fix

--- Assets/Scripts/ShotController.cs
+++ Assets/Scripts/ShotController.cs
@@ -7,48 +7,34 @@ public class ShotController : MonoBehaviour
     public SpaceCraft spaceCraft; 
 
     public GameObject rocket; // rocket template to create prefab
-    private GameObject rocketClone; // instantiated rocket
+    [HideInInspector] public GameObject rocketClone; // instantiated rocket
 
     private float clickClock; // clock that counts for next click after initial click
     private float flyTime; // represents force fly time of instantiated rocked
     private int shotCaller; // int to instantiate rocket if 2nd click occurs at the time
-    private bool isFired; // to check if rocket is fired in the interval
 
-    private Collider2D rocketCollider; // to adjust isTrigger of the object
     private Vector3 rocketVelocity; // to determine the speed of rocket 
-
-    private GameObject[] rocks; // rocks in the map
-    private float[] rockCoordinates; // rock coordinates for x-axis
+    //[HideInInspector] public bool isRocket; // to determine if the rocket is present or not
 
     // try to launch the rocket inside of the spacecraft trigger off/on can be the trick
     void Start()
-    {
-        rocks = GameObject.FindGameObjectsWithTag("Obstacle");
-        rockCoordinates = new float[rocks.Length]; // set the length of the array
+    {      
 
-        for (int i = 0; i < rocks.Length; i++)
-            rockCoordinates[i] = 0f;
-
-        for (int i = 0; i < rocks.Length; i++) // to assign initial coordinates of y-axis
-            rockCoordinates[i] = rocks[i].transform.position.y;
     }
 
     // double click rocket initialization works little bit problematic 
     void Update()
     {
         if (Input.touchCount > 0)
-            if (shotCaller >= 2 && !isFired && spaceCraft.isRightLeft)
+            if (shotCaller >= 2 && spaceCraft.isRightLeft)
             { // if another click occurs at the short time interval
               // Instantiate at position (0, 0, 0) and zero rotation.
                 if (flyTime == 0f)
                 {
                     rocketClone = Instantiate(rocket, transform.position, Quaternion.identity); // + (transform.up)
-                    rocketCollider = rocketClone.GetComponent<Collider2D>();
-                    rocketCollider.isTrigger = true; // for rocket to not stuck inside of spacecraft
                 }
 
-                isFired = true;
-                flyTime += Time.deltaTime;
+                flyTime += Time.deltaTime; // to break the equality of flytime with zero
             }
 
         clickClock += Time.deltaTime;
@@ -58,12 +44,9 @@ public class ShotController : MonoBehaviour
             flyTime += Time.deltaTime;
             if (flyTime < 0.5f) // for more realistic acceleration and speed, adjust time and constant below
             { // add the current velocity of spacecraft and launching speed 
-                rocketVelocity = new Vector3(0f, (spaceCraft.currentVelocity + 10) * (Time.deltaTime*100), -0.5f); // seems fine
-                rocketClone.GetComponent<Rigidbody2D>().AddForce(rocketVelocity); // (Mathf.Log(spaceCraft.currentVelocity, 1.5f)
+                rocketVelocity = new Vector3(0f, (spaceCraft.currentVelocity + 10) * (Time.deltaTime * 100), -0.5f); // seems fine
+                rocketClone.GetComponent<Rigidbody2D>().AddForce(rocketVelocity); // (Mathf.Log(spaceCraft.currentVelocity, 1.5f)               
             }
-            print(transform.up);
-            if (flyTime > 0.5f && rocketCollider != null)
-                rocketCollider.isTrigger = false; // trigger off when rocket leaves the spacecraft entirely
         }
 
         if (Input.touchCount > 0)
@@ -76,25 +59,70 @@ public class ShotController : MonoBehaviour
         {
             shotCaller = 0;
             clickClock = 0f;
-            isFired = false;
         }
 
-        if (flyTime > 5f) // clock for rockets to fire them only once in a five seconds
+        if (flyTime > 3f) // clock for rockets to fire them only once in a five seconds
+        {
+            Destroy(rocketClone); // here it destroys the clone and in collision method it destroys the rocket itself
             flyTime = 0f;
+        }
     }
+
+    //void RockSetter()
+    //{
+    //    rockinTime += Time.deltaTime; // 
+
+    //    while (rockinTime < 0.4f) // unless timer is sooner than .4 seconds
+    //    {
+    //        for (int i = 0; i < rocks.Length; i++) // gives nullreference error in collisionenter
+    //        {
+    //            if (rockCoordinates[i] != rocks[i].transform.position.y)
+    //            {
+    //                if (isRocket) // destroy the rocket if there is any
+    //                {
+    //                    Destroy(rocketClone); // destroy if rocket changes position of any rock
+    //                    isRocket = false;
+    //                }
+
+    //                Destroy(rocks[i]); // destroy the rock that is moving
+    //                rocks = GameObject.FindGameObjectsWithTag("Obstacle"); // rearrange the rock array
+    //            }
+    //        }
+    //    }
+
+    //    rockinTime = 0f; // reset the timer
+    //}
+
+    //void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    rockinTime += Time.deltaTime; // 
+
+    //    while (rockinTime < 0.4f) // unless timer is sooner than .4 seconds
+    //    {
+    //            for (int i = 0; i < rocks.Length; i++) // gives nullreference error in collisionenter
+    //            {
+    //                if (rockCoordinates[i] != rocks[i].transform.position.y)
+    //                {
+    //                    if (isRocket) // destroy the rocket if there is any
+    //                    {
+    //                        Destroy(rocketClone); // destroy if rocket changes position of any rock
+    //                        isRocket = false;
+    //                    }
+
+    //                    Destroy(rocks[i]); // destroy the rock that is moving
+    //                    rocks = GameObject.FindGameObjectsWithTag("Obstacle"); // rearrange the rock array
+    //                }
+    //            }
+    //    }
+
+    //    rockinTime = 0f; // reset the timer
+    //}
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Obstacle")
-        {
-            Destroy(GameObject.FindWithTag("Obstacle"));
-        }
-
-        for (int i = 0; i < rocks.Length; i++) // gives nulreference error
-        {
-            print(rocks[i]);
-            if (rockCoordinates[i] != rocks[i].transform.position.y)
-                Destroy(rocks[i]);
-        }
+        // if the rocket hits a rock, destroy the rocket and the object collided
+        Destroy(collision.collider.gameObject);
+        Destroy(rocket); // it seems there is no problem
+        //print(collision.collider.gameObject);
     }
 }
