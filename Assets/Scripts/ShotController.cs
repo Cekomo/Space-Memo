60b750b0716c3338140982f3922be91776e2d54c
Cekomo <cemil.sahin@ozu.edu.tr>
2022-08-28 17:40:07 +0300

    rocket launcher button implementation

--- Assets/Scripts/ShotController.cs
+++ Assets/Scripts/ShotController.cs
@@ -11,27 +11,33 @@ public class ShotController : MonoBehaviour
 
     private float clickClock; // clock that counts for next click after initial click
     private float flyTime; // represents force fly time of instantiated rocked
-    private int shotCaller; // int to instantiate rocket if 2nd click occurs at the time
+    //private int shotCaller; // int to instantiate rocket if 2nd click occurs at the time
 
     private Vector3 rocketVelocity; // to determine the speed of rocket 
     //[HideInInspector] public bool isRocket; // to determine if the rocket is present or not
+    private bool isButton = false; // determines if the button is active
+    public CanvasGroup rocketButton; // to manipulate the button during play-time
 
     // try to launch the rocket inside of the spacecraft trigger off/on can be the trick
     void Start()
     {      
-
+         
+        // i am going to fix the small bugs 
+        // rocket should move with some kind of animation
     }
 
     // double click rocket initialization works little bit problematic 
     void Update()
     {
+        print(isButton);
         if (Input.touchCount > 0)
-            if (shotCaller >= 2 && spaceCraft.isRightLeft)
+            if (isButton && spaceCraft.isRightLeft) // (shotCaller >= 2 || isButton) && spaceCraft.isRightLeft
             { // if another click occurs at the short time interval
               // Instantiate at position (0, 0, 0) and zero rotation.
                 if (flyTime == 0f)
                 {
                     rocketClone = Instantiate(rocket, transform.position, Quaternion.identity); // + (transform.up)
+                    isButton = false;
                 }
 
                 flyTime += Time.deltaTime; // to break the equality of flytime with zero
@@ -49,22 +55,34 @@ public class ShotController : MonoBehaviour
             }
         }
 
-        if (Input.touchCount > 0)
-            if (Input.GetTouch(0).phase == TouchPhase.Began)
-            {  
-                shotCaller++;
-            }      
+        // below two if statements is for double-click rocket launcher property
+        //if (Input.touchCount > 0)
+        //    if (Input.GetTouch(0).phase == TouchPhase.Began)
+        //    {  
+        //        shotCaller++;
+        //    }      
 
-        if (clickClock > 0.35f) // reset clock for next possibility
-        {
-            shotCaller = 0;
-            clickClock = 0f;
-        }
+        //if (clickClock > 0.35f) // reset clock for next possibility
+        //{
+        //    shotCaller = 0;
+        //    clickClock = 0f;
+        //}
 
-        if (flyTime > 3f) // clock for rockets to fire them only once in a five seconds
+        if (flyTime > 5f) // clock for rockets to fire them only once in a five seconds
         {
             Destroy(rocketClone); // here it destroys the clone and in collision method it destroys the rocket itself
             flyTime = 0f;
+            rocketButton.alpha = 1f;
+        }
+    }
+
+
+    public void RocketLauncher()
+    {
+        if (flyTime == 0f)
+        {
+            isButton = true; // make the button available after 5 seconds
+            rocketButton.alpha = 0.4f;
         }
     }
 
@@ -119,10 +137,10 @@ public class ShotController : MonoBehaviour
     //}
 
     void OnCollisionEnter2D(Collision2D collision)
-    {
+    {        
         // if the rocket hits a rock, destroy the rocket and the object collided
         Destroy(collision.collider.gameObject);
         Destroy(rocket); // it seems there is no problem
-        //print(collision.collider.gameObject);
+        //print(collision.collider.gameObject);       
     }
 }
--- Assets/Scripts/SpaceCraft.cs
+++ Assets/Scripts/SpaceCraft.cs
@@ -18,6 +18,10 @@ public class SpaceCraft : MonoBehaviour
     //[HideInInspector] public bool isRight; // move the spacecraft right if the variable is true
     //private bool isUp; // force up the spacecraft if the variable is true
     [HideInInspector] public bool isRightLeft; // first up function must be triggered to go left/right
+    private bool isRight; // to move towards rigth only during single finger touch 
+    private bool isLeft; // to move towards left only during single finger touch
+    private bool isUp;  // to move towards rigth only during single finger touch
+
     private bool isCounterMove; // to return back of the spacecraft when it exceeds borders
 
     private int screenX = Screen.width; // to determine swipe action on x-axis
@@ -59,30 +63,31 @@ public class SpaceCraft : MonoBehaviour
          
         if (Input.touchCount > 0 && !isFinished)
         {
-            if (Input.touches[0].position.x - startPos.x >= screenX / 5 && player.transform.position.x < 1.7f && isRightLeft)
+            if (Input.touches[0].position.x - startPos.x >= screenX / 5 && player.transform.position.x < 1.7f && isRightLeft && isRight)
             {
                 p_RigidBody.AddForce(transform.right * moveSpeed / 3);
                 //transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
-                //isRight = true;
+                isLeft = false; isUp = false;
             }
-            else if (Input.touches[0].position.x - startPos.x <= -screenX / 5 && player.transform.position.x > -1.7f && isRightLeft)
+            else if (Input.touches[0].position.x - startPos.x <= -screenX / 5 && player.transform.position.x > -1.7f && isRightLeft && isLeft)
             {
                 p_RigidBody.AddForce(transform.right * -moveSpeed / 3);
                 //transform.Translate(Vector2.right * -moveSpeed * Time.deltaTime);
-                //isLeft = true;
+                isRight = false; isUp = false;
             }
-            else if (Input.touches[0].position.y - startPos.y >= screenY / 6 && player.transform.position.y < 32f) // force up if swipe is along one sixth the screen
+            else if (Input.touches[0].position.y - startPos.y >= screenY / 6 && player.transform.position.y < 32f && isUp) // force up if swipe is along one sixth the screen
             {
                 p_RigidBody.AddForce(transform.up * moveSpeed / 3);
 
                 isRightLeft = true;
                 currentVelocity = p_RigidBody.velocity.magnitude;
+                isRight = false; isLeft = false;
             }
         }
 
         if (Input.touchCount == 0 || Mathf.Abs(player.transform.position.x) > 1.7f && !isFinished)
         {
-            speedFading = p_RigidBody.velocity.x * 0.96f; // speed decreases cumulatively (multiplied with 0.98 continuously)
+            speedFading = p_RigidBody.velocity.x * 0.96f; // speed decreases cumulatively (multiplied with 0.96  continuously)
             p_RigidBody.velocity = new Vector2(speedFading, currentVelocity); // omitting time.deltatime solves background speed problem
             //transform.Translate(Vector2.up * currentVelocity * Time.deltaTime);
             //isRight = false; isLeft = false;
@@ -90,7 +95,12 @@ public class SpaceCraft : MonoBehaviour
                 isCounterMove = true; // make the counter move available if velocity is zero out of the border
         }
 
-        if (Input.touchCount == 0 && isCounterMove && Mathf.Abs(player.transform.position.x) > 1.7f)
+        if (Input.touchCount == 0 && (!isUp || !isRight || !isLeft)) // to make them all true for the next move
+        {
+            isUp = true; isLeft = true; isRight = true;
+        }
+
+        if (isCounterMove && Mathf.Abs(player.transform.position.x) > 1.7f)
         { // if the spacecraft exceeds the border on x-axis, turn back inside of the border
             if (player.transform.position.x < -1.7f) // if left border exceeds
                 transform.Translate(Vector2.right * 0.5f * Time.deltaTime);
